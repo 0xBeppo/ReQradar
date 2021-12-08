aa5008620066b0576180e5573e24d151f67eb9e4
Markel Elorza <melorzaalvarez@gmail.com>
2021-12-08 19:33:03 +0100

    Creation and deployment of some linux os log sources

--- ReQradar.py
+++ ReQradar.py
@@ -14,7 +14,11 @@ from api import API
 
 def run():
     api = API()
-    print(api.get_log_sources())
+    api.post_log_sources("webserverRsyslog", "Rsyslog source of webserver", "webserver", "Linux OS")
+    api.post_log_sources("databaseRsyslog", "Rsyslog source of database", "database", "Linux OS")
+    api.post_log_sources("firewallRsyslog", "Rsyslog source of firewall", "firewall", "Linux OS")
+    api.post_log_sources("rabbitmqRsyslog", "Rsyslog source of rabbitmq", "rabbitmq", "Linux OS")
+    api.post_deploy()
 
 if __name__ == "__main__":
     run()
